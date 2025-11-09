2104e38f88910b02566e36951341ff50c3d34708
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-09 15:35:55 +0700

    upd

---
+++ config.js
@@ -0,0 +1,7 @@
+const productConfig = {
+    product1: 'https://trnsfx.ru/go/ss1i1jzx2w',
+    product2: 'https://trnsfx.ru/go/sck5upjito',
+    product3: 'https://trnsfx.ru/go/s960f2cnox',
+    product4: 'https://trnsfx.ru/go/snxajovdvn'
+};
+
